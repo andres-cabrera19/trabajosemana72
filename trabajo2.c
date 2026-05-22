9df037e32a696cca4b3765f038c93ba2bb1c834d
Andres Cabrera <andres.cabrera.calupina@udla.edu.ec>
2026-05-22 14:54:44 -0500

    Aplicacion de sieof y promedio de notas

--- trabajo2.c
+++ trabajo2.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-float notas[] = {6.5, 7.1, 6, 5, 8.1};
+float notas[] = {6.5, 7.1, 6, 5, 6.1};
+float suma = 0;
+int n = sizeof (notas) / sizeof (notas[0]);
 int main(){
    for (int i = 0; i < 5; i++){
        printf("%.1f\n", notas[i]);
+       suma = suma + notas[i];
    }
+   printf("El promedio es %.2f", suma/n);
    return 0;
 }
