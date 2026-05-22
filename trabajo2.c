8b190d681f5c977efc0ef0ab62ad2500e3d6ba1a
Andres Cabrera <andres.cabrera.calupina@udla.edu.ec>
2026-05-22 15:18:54 -0500

    paso de parametros x referencia

--- trabajo2.c
+++ trabajo2.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+float calcular_promedio(float *a); //a = suma total
 float notas[] = {6.5, 7.1, 6, 5, 6.1};
-float suma = 0;
 int n = sizeof (notas) / sizeof (notas[0]);
 int main(){
-   for (int i = 0; i < 5; i++){
-       printf("%.1f\n", notas[i]);
-       suma = suma + notas[i];
-   }
-   printf("El promedio es %.2f", suma/n);
+   //printf("El promedio es %.2f", suma/n);
+   printf("\nEl promedio puntero -> %.2f", calcular_promedio(notas));
    return 0;
+}
+float calcular_promedio(float *a) {
+   float suma = 0;
+   for (int i = 0; i < 5; i++){
+       printf("%.1f\n", a[i]);
+       suma = suma + a[i];
+   }
+   return suma / 5;
 }
